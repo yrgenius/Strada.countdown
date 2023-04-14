7ba38e3e9b11ee183ee3251dfe89f003768c12a0
yrgenius <yrgenius80@gmail.com>
2023-04-14 11:57:08 +0900

    start project

---
+++ index.js
@@ -0,0 +1,15 @@
+import { formatDistance, subDays } from "date-fns";
+
+const input = document.querySelector('.form__input');
+const button = document.querySelector('.form__btn');
+const out = document.querySelector('.text');
+
+button.addEventListener('click', buttonHandler);
+
+function buttonHandler() {
+    event.preventDefault();
+
+    const selectDate = input.value | Date.now();
+    out.textContent = selectDate;
+    console.log(selectDate); //del
+}
